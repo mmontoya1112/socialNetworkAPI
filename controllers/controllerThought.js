8d3fdb935baa1d2901df9632440deb72f76f9743
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-10-12 15:02:26 -0700

    added thought controllers

---
+++ controllers/controllerThought.js
@@ -0,0 +1,39 @@
+const { User, Thought } = require('../models');
+const { ObjectId } = require('mongoose').Types;
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a thought
+  getSingleThought(req, res) {
+    Thought.findById({ _id: req.params.thoughtId })
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+  },
+  // Create a thought
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a thought
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+    .then(() => {res.json("Thought was deleted.")})
+        .catch((err) => res.status(500).json(err));
+  },
+  // Update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { new: true }
+    )
+    .then(() => {res.json("updated thought")})
+    .catch((err) => res.status(500).json(err));
+  },
+};
